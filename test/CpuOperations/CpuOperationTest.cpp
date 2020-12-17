fd0ac74b7dab630480ac07d045c2a88dd89e1c53
Paul Trampert <paul.trampert@ptrampert.com>
2020-12-16 23:58:08 -0500

    Add quick test around getting operations.

---
+++ test/CpuOperations/CpuOperationTest.cpp
@@ -0,0 +1,19 @@
+//
+// Created by paul.trampert on 12/16/2020.
+//
+
+#include <gtest/gtest.h>
+#include <GenieSys/CpuOperations/CpuOperation.h>
+
+TEST(CpuOperationTest, getOperationsReturnsOperationsInTheCorrectOrder) {
+    Bus bus;
+    M68kCpu* cpu = bus.getCpu();
+    std::shared_ptr<CpuOperation> previous = nullptr;
+    auto operations = getOperations(cpu, &bus);
+    for(auto & op : operations) {
+        if (previous != nullptr) {
+            ASSERT_TRUE(previous->getSpecificity() >= op->getSpecificity());
+        }
+        previous = op;
+    }
+}
