29157fbf31d515d13886a25441b581345b32ed55
Paul Trampert <paul.trampert@ptrampert.com>
2021-01-13 20:41:33 -0500

    Revert extraction of helper

--- src/CpuOperations/Add.cpp
+++ src/CpuOperations/Add.cpp
@@ -4,7 +4,6 @@
 #include <GenieSys/AddressingModes/AddressingMode.h>
 #include <GenieSys/CpuOperations/Add.h>
 #include <GenieSys/BitMask.h>
-#include <GenieSys/buildCcrFlags.h>
 #include <cmath>
 #include <GenieSys/getPossibleOpcodes.h>
 #include <sstream>
@@ -20,7 +19,25 @@ static BitMask<uint16_t> SIZE = BitMask<uint16_t>(7, 2, 0, 2);
 static BitMask<uint16_t> EA_MODE = BitMask<uint16_t>(5, 3);
 static BitMask<uint16_t> EA_REG = BitMask<uint16_t>(2, 3);
 
-
+template<class U, class S>
+static uint8_t getCcrFlags(U result, U op1, U op2) {
+    uint8_t ccr = 0;
+    if (result < op1) {
+        ccr |= CCR_OVERFLOW;
+    }
+    if (((S) result < 0 && (S)op1 > 0 && (S)op2 > 0) ||
+        ((S) result > 0 && (S)op1 < 0 && (S)op2 < 0)
+            ) {
+        ccr |= CCR_EXTEND | CCR_CARRY;
+    }
+    if (result == 0) {
+        ccr |= CCR_ZERO;
+    }
+    if ((S)result < 0) {
+        ccr |= CCR_NEGATIVE;
+    }
+    return ccr;
+}
 
 Add::Add(M68kCpu *cpu, Bus *bus) : CpuOperation(cpu, bus) {
 
@@ -63,7 +80,7 @@ void Add::addBytes(uint8_t direction, uint8_t dataRegAddr, AddressingResult *eaR
     uint8_t regOp = cpu->getDataRegister(dataRegAddr) & 0x000000FF;
     uint8_t eaOp = eaResult->getDataAsByte();
     uint8_t result = direction == 1 ? regOp + eaOp : eaOp + regOp;
-    uint8_t ccr = buildCcrFlags<uint8_t, int8_t>(result, regOp, eaOp);
+    uint8_t ccr = getCcrFlags<uint8_t, int8_t>(result, regOp, eaOp);
     cpu->setCcrFlags(ccr);
     direction == 1 ? eaResult->write(result) : cpu->setDataRegister(dataRegAddr, result);
 }
@@ -72,7 +89,7 @@ void Add::addWords(uint8_t direction, uint8_t dataRegAddr, AddressingResult *eaR
     uint16_t regOp = cpu->getDataRegister(dataRegAddr) & 0x0000FFFF;
     uint16_t eaOp = eaResult->getDataAsWord();
     uint16_t result = direction == 1 ? regOp + eaOp : eaOp + regOp;
-    uint8_t ccr = buildCcrFlags<uint16_t, int16_t>(result, regOp, eaOp);
+    uint8_t ccr = getCcrFlags<uint16_t, int16_t>(result, regOp, eaOp);
     cpu->setCcrFlags(ccr);
     direction == 1 ? eaResult->write(result) : cpu->setDataRegister(dataRegAddr, result);
 }
@@ -81,7 +98,7 @@ void Add::addLongs(uint8_t direction, uint8_t dataRegAddr, AddressingResult *eaR
     uint32_t regOp = cpu->getDataRegister(dataRegAddr);
     uint32_t eaOp = eaResult->getDataAsWord();
     uint32_t result = direction == 1 ? regOp + eaOp : eaOp + regOp;
-    uint8_t ccr = buildCcrFlags<uint32_t, int32_t>(result, regOp, eaOp);
+    uint8_t ccr = getCcrFlags<uint32_t, int32_t>(result, regOp, eaOp);
     cpu->setCcrFlags(ccr);
     direction == 1 ? eaResult->write(result) : cpu->setDataRegister(dataRegAddr, result);
 }
