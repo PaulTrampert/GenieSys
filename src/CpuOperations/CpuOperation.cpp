4a93b1febbfc96a51d520091fd99f509630ecfd1
Paul Trampert <paul.trampert@ptrampert.com>
2021-02-23 19:23:10 -0500

    Add missing ops to CpuOperation.cpp.

--- src/CpuOperations/CpuOperation.cpp
+++ src/CpuOperations/CpuOperation.cpp
@@ -9,6 +9,11 @@
 #include <GenieSys/CpuOperations/ORItoCCR.h>
 #include <GenieSys/CpuOperations/ORItoSR.h>
 #include <GenieSys/CpuOperations/ANDI.h>
+#include <GenieSys/CpuOperations/ADDI.h>
+#include <GenieSys/CpuOperations/SUBI.h>
+#include <GenieSys/CpuOperations/ANDItoCCR.h>
+#include <GenieSys/CpuOperations/ANDItoSR.h>
+#include <GenieSys/CpuOperations/Nop.h>
 
 CpuOperation::CpuOperation(M68kCpu *cpu, Bus *bus) {
     this->cpu = cpu;
@@ -19,9 +24,14 @@ std::vector<std::shared_ptr<CpuOperation>> getOperations(M68kCpu *cpu, Bus *bus)
     std::vector<std::shared_ptr<CpuOperation>> operations{
             std::shared_ptr<CpuOperation>(new Abcd(cpu, bus)),
             std::shared_ptr<CpuOperation>(new Add(cpu, bus)),
+            std::shared_ptr<CpuOperation>(new ADDI(cpu, bus)),
             std::shared_ptr<CpuOperation>(new ANDI(cpu, bus)),
+            std::shared_ptr<CpuOperation>(new ANDItoCCR(cpu, bus)),
+            std::shared_ptr<CpuOperation>(new ANDItoSR(cpu, bus)),
+            std::shared_ptr<CpuOperation>(new Nop(cpu, bus)),
             std::shared_ptr<CpuOperation>(new ORItoCCR(cpu, bus)),
-            std::shared_ptr<CpuOperation>(new ORItoSR(cpu, bus))
+            std::shared_ptr<CpuOperation>(new ORItoSR(cpu, bus)),
+            std::shared_ptr<CpuOperation>(new SUBI(cpu, bus))
     };
 
     std::sort(operations.begin(), operations.end(), compare);
