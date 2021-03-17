808944d1935def1e76f290ec23b84b7651db2425
Paul Trampert <paul.trampert@ptrampert.com>
2021-03-16 22:13:21 -0400

    Implement CMPI.

---
+++ src/CpuOperations/CMPI.cpp
@@ -0,0 +1,83 @@
+//
+// Created by paul.trampert on 12/25/2020.
+//
+
+#include <GenieSys/CpuOperations/CMPI.h>
+#include <GenieSys/getPossibleOpcodes.h>
+#include <GenieSys/AddressingModes/AddressingMode.h>
+#include <GenieSys/AddressingModes/DataRegisterDirectMode.h>
+#include <GenieSys/AddressingModes/AddressRegisterDirectMode.h>
+#include <GenieSys/AddressingModes/ProgramCounterAddressingMode.h>
+#include <GenieSys/AddressingModes/ImmediateDataMode.h>
+#include <GenieSys/getCcrFlags.h>
+#include <sstream>
+#include <cmath>
+
+CMPI::CMPI(M68kCpu *cpu, Bus *bus) : CpuOperation(cpu, bus) {
+
+}
+
+std::vector<uint16_t> CMPI::getOpcodes() {
+    return getPossibleOpcodes(BASE_OPCODE, std::vector<BitMask<uint16_t>*>{
+        &sizeMask,
+        &eaModeMask,
+        &eaRegMask,
+    });
+}
+
+uint8_t CMPI::getSpecificity() {
+    return sizeMask.getWidth() + eaModeMask.getWidth() + eaRegMask.getWidth();
+}
+
+uint8_t CMPI::execute(uint16_t opWord) {
+    uint8_t size = pow(2, sizeMask.apply(opWord));
+    uint8_t eaModeCode = eaModeMask.apply(opWord);
+    uint8_t eaReg = eaRegMask.apply(opWord);
+    AddressingMode* immMode = cpu->getAddressingMode(ProgramCounterAddressingMode::MODE_ID);
+    auto immData = immMode->getData(ImmediateDataMode::MODE_ID, size);
+    AddressingMode* eaMode = cpu->getAddressingMode(eaModeCode);
+    auto eaResult = eaMode->getData(eaReg, size);
+    bool isMemory = eaModeCode != DataRegisterDirectMode::MODE_ID && eaModeCode != AddressRegisterDirectMode::MODE_ID;
+    uint8_t baseCycles = 1;
+    uint8_t byteResult;
+    uint16_t wordResult;
+    uint32_t longResult;
+    uint8_t newCcrFlags;
+    switch(size) {
+        case 1:
+            baseCycles = isMemory ? 8 : 8;
+            byteResult = eaResult->getDataAsByte() - immData->getDataAsByte();
+            newCcrFlags = getSubtractionCcrFlags<uint8_t, int8_t>(byteResult, eaResult->getDataAsByte(),
+                                                                  immData->getDataAsByte());
+            cpu->setCcrFlags(newCcrFlags);
+            break;
+        case 2:
+            baseCycles = isMemory ? 8 : 8;
+            wordResult = eaResult->getDataAsWord() - immData->getDataAsWord();
+            newCcrFlags = getSubtractionCcrFlags<uint16_t, int16_t>(wordResult, eaResult->getDataAsWord(),
+                                                                    immData->getDataAsWord());
+            cpu->setCcrFlags(newCcrFlags);
+            break;
+        case 4:
+            baseCycles = isMemory ? 12 : 14;
+            longResult = eaResult->getDataAsLong() - immData->getDataAsLong();
+            newCcrFlags = getSubtractionCcrFlags<uint32_t, int32_t>(longResult, eaResult->getDataAsLong(),
+                                                                    immData->getDataAsLong());
+            cpu->setCcrFlags(newCcrFlags);
+            break;
+        default:
+            break;
+    }
+    return baseCycles + eaResult->getCycles();
+}
+
+std::string CMPI::disassemble(uint16_t opWord) {
+    uint8_t size = pow(2, sizeMask.apply(opWord));
+    uint8_t eaModeCode = eaModeMask.apply(opWord);
+    uint8_t eaReg = eaRegMask.apply(opWord);
+    AddressingMode* immMode = cpu->getAddressingMode(ProgramCounterAddressingMode::MODE_ID);
+    AddressingMode* eaMode = cpu->getAddressingMode(eaModeCode);
+    std::stringstream stream;
+    stream << "CMPI " << immMode->disassemble(ImmediateDataMode::MODE_ID, size) << "," << eaMode->disassemble(eaReg, size);
+    return stream.str();
+}
---
+++ test/CpuOperations/CMPITest.cpp
@@ -0,0 +1,72 @@
+//
+// Created by paul.trampert on 12/25/2020.
+//
+
+#include <gtest/gtest.h>
+#include <GenieSys/CpuOperations/CMPI.h>
+
+static uint16_t BASE_OPCODE = 0b0000010000000000;
+
+struct CMPITest : testing::Test {
+    M68kCpu* cpu;
+    Bus bus;
+    CMPI* subject;
+
+    CMPITest() {
+        cpu = bus.getCpu();
+        subject = new CMPI(cpu, &bus);
+    }
+
+    ~CMPITest() override {
+        delete subject;
+    }
+};
+
+TEST_F(CMPITest, ItComparesBytes) {
+    cpu->setPc(300);
+    bus.writeWord(300, 0x00F0);
+    bus.writeByte(600, 0xAB);
+    cpu->setAddressRegister(0, 600);
+    uint8_t cycles = subject->execute(0b0000110000010000);
+    ASSERT_EQ(12, cycles);
+    ASSERT_EQ(0xAB, bus.read(600));
+    ASSERT_EQ(CCR_OVERFLOW | CCR_NEGATIVE, cpu->getCcrFlags());
+}
+
+TEST_F(CMPITest, ItComparesWords) {
+    cpu->setPc(300);
+    bus.writeWord(300, 0x00F0);
+    bus.writeWord(600, 0x01AB);
+    cpu->setAddressRegister(0, 600);
+    uint8_t cycles = subject->execute(0b0000110001010000);
+    ASSERT_EQ(12, cycles);
+    ASSERT_EQ(0x01AB, bus.readWord(600));
+    ASSERT_EQ(0, cpu->getCcrFlags());
+}
+
+TEST_F(CMPITest, ItComparesLongs) {
+    cpu->setPc(300);
+    bus.writeLong(300, 0x010100F0);
+    bus.writeLong(600, 0x010101AB);
+    cpu->setAddressRegister(0, 600);
+    uint8_t cycles = subject->execute(0b0000110010010000);
+    ASSERT_EQ(20, cycles);
+    ASSERT_EQ(0x010101AB, bus.readLong(600));
+    ASSERT_EQ(0, cpu->getCcrFlags());
+}
+
+TEST_F(CMPITest, GetOpcodesOnlyReturnsPossibleOpcodes) {
+    auto opcodes = subject->getOpcodes();
+    for(auto opcode : opcodes) {
+        ASSERT_EQ(0b0000110000000000, 0b0000110000000000 & opcode);
+    }
+}
+
+TEST_F(CMPITest, TestDisassemble) {
+    cpu->setPc(300);
+    bus.writeWord(300, 0x00F0);
+    bus.writeByte(600, 0xAB);
+    cpu->setAddressRegister(0, 600);
+    std::string assembly = subject->disassemble(0b0000110000010000);
+    ASSERT_EQ("CMPI $f0,(A0)", assembly);
+}
