c7fcf3b799ad49563383cff89fe9831bd41c2b89
Paul Trampert <paul.trampert@ptrampert.com>
2021-04-05 00:28:54 -0400

    Implement disassemble.

--- src/CpuOperations/BTST.cpp
+++ src/CpuOperations/BTST.cpp
@@ -4,6 +4,11 @@
 
 #include <GenieSys/CpuOperations/BTST.h>
 #include <GenieSys/getPossibleOpcodes.h>
+#include <sstream>
+#include <GenieSys/AddressingModes/AddressingMode.h>
+#include <GenieSys/AddressingModes/ImmediateDataMode.h>
+#include <GenieSys/AddressingModes/ProgramCounterAddressingMode.h>
+#include <GenieSys/AddressingModes/DataRegisterDirectMode.h>
 
 BTST::BTST(M68kCpu *cpu, Bus *bus) : CpuOperation(cpu, bus) {
 
@@ -36,5 +41,20 @@ uint8_t BTST::execute(uint16_t opWord) {
 }
 
 std::string BTST::disassemble(uint16_t opWord) {
-    return std::string();
+    bool immMode = !ImmMask.apply(opWord);
+    uint8_t eaModeId = EaModeMask.apply(opWord);
+    uint8_t eaAddr = EaAddrMask.apply(opWord);
+    uint8_t destSize = eaModeId == DataRegisterDirectMode::MODE_ID ? 4 : 1;
+    auto eaMode = cpu->getAddressingMode(eaModeId);
+    std::stringstream stream;
+    if (immMode) {
+        auto imm = cpu->getAddressingMode(ProgramCounterAddressingMode::MODE_ID);
+        auto destData = eaMode->disassemble(eaAddr, destSize);
+        stream << "BTST " << imm->disassemble(ImmediateDataMode::MODE_ID, 1) << "," << destData;
+    }
+    else {
+        auto dn = cpu->getAddressingMode(DataRegisterDirectMode::MODE_ID);
+        stream << "BTST " << dn->disassemble(DnMask.apply(opWord), 4) << "," << eaMode->disassemble(eaAddr, destSize);
+    }
+    return stream.str();
 }
--- test/CpuOperations/BTSTTest.cpp
+++ test/CpuOperations/BTSTTest.cpp
@@ -24,3 +24,14 @@ struct BTSTTest : testing::Test {
 TEST_F(BTSTTest, SpecificityIsTen) {
     ASSERT_EQ(10, subject->getSpecificity());
 }
+
+TEST_F(BTSTTest, DisassembleImmediate) {
+    cpu->setPc(20);
+    bus.writeWord(20, 0x0003);
+
+    ASSERT_EQ("BTST $03,D7", subject->disassemble(0b0000100000000111));
+}
+
+TEST_F(BTSTTest, DisassembleDataRegister) {
+    ASSERT_EQ("BTST D2,D7", subject->disassemble(0b0000010100000111));
+}
