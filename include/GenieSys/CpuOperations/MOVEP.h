ebff871027487afaa9cd021e2091ac16cd0cbd4a
Paul Trampert <paul.trampert@ptrampert.com>
2021-09-17 00:27:39 -0400

    Implement MOVEP.execute()

--- include/GenieSys/CpuOperations/MOVEP.h
+++ include/GenieSys/CpuOperations/MOVEP.h
@@ -18,6 +18,12 @@ private:
     BitMask<uint16_t> OpModeMask = BitMask<uint16_t>(8, 3, WORD_MEM_REG, LONG_REG_MEM);
     BitMask<uint16_t> DnMask = BitMask<uint16_t>(11, 3);
     BitMask<uint16_t> AnMask = BitMask<uint16_t>(2, 3);
+
+    void WordMemToReg(uint32_t anAddr, uint8_t dn);
+    void LongMemToReg(uint32_t anAddr, uint8_t dn);
+    void WordRegToMem(uint32_t anAddr, uint8_t dn);
+    void LongRegToMem(uint32_t anAddr, uint8_t dn);
+
 public:
     MOVEP(M68kCpu* cpu, Bus* bus);
     std::string disassemble(uint16_t opWord) override;
--- src/CpuOperations/MOVEP.cpp
+++ src/CpuOperations/MOVEP.cpp
@@ -4,17 +4,39 @@
 #include <GenieSys/CpuOperations/MOVEP.h>
 #include <GenieSys/getPossibleOpcodes.h>
 #include <vector>
+#include <GenieSys/AddressingModes/AddressRegisterIndirectDisplacementMode.h>
 
 MOVEP::MOVEP(M68kCpu *cpu, Bus *bus) : CpuOperation(cpu, bus) {
 
 }
+
+
 
 std::string MOVEP::disassemble(uint16_t opWord) {
     return std::string();
 }
 
 uint8_t MOVEP::execute(uint16_t opWord) {
-    return 0;
+    auto dn = DnMask.apply(opWord);
+    auto opMode = (OpMode)OpModeMask.apply(opWord);
+    auto addrMode = cpu->getAddressingMode(AddressRegisterIndirectDisplacementMode::MODE_ID);
+    auto an = AnMask.apply(opWord);
+    auto anAddr = addrMode->getAddress(an);
+    switch (opMode) {
+        case LONG_MEM_REG:
+            LongMemToReg(anAddr, dn);
+            break;
+        case LONG_REG_MEM:
+            LongRegToMem(anAddr, dn);
+            break;
+        case WORD_MEM_REG:
+            WordMemToReg(anAddr, dn);
+            break;
+        case WORD_REG_MEM:
+            WordRegToMem(anAddr, dn);
+            break;
+    }
+    return opMode == LONG_REG_MEM || opMode == LONG_MEM_REG ? 24 : 16;
 }
 
 std::vector<uint16_t> MOVEP::getOpcodes() {
@@ -27,4 +49,35 @@ std::vector<uint16_t> MOVEP::getOpcodes() {
 
 uint8_t MOVEP::getSpecificity() {
     return DnMask.getWidth() + AnMask.getWidth() + OpModeMask.getWidth();
+}
+
+void MOVEP::WordMemToReg(uint32_t anAddr, uint8_t dn) {
+    uint16_t dnData = ((uint16_t) bus->read(anAddr) << 8) + bus->read(anAddr + 2);
+    cpu->setDataRegister(dn, dnData);
+}
+
+void MOVEP::LongMemToReg(uint32_t anAddr, uint8_t dn) {
+    uint32_t dnData = 0;
+    for (int i = 0; i < 4; i++) {
+        uint32_t shift = (3 - i) * 8;
+        uint32_t readAddr = anAddr + (2 * i);
+        dnData |= (bus->read(readAddr) << shift);
+    }
+    cpu->setDataRegister(dn, dnData);
+}
+
+void MOVEP::WordRegToMem(uint32_t anAddr, uint8_t dn) {
+    uint16_t dnData = (cpu->getDataRegister(dn) & 0x0000FFFF);
+    bus->writeByte(anAddr, ((dnData & 0xFF00) >> 8));
+    bus->writeByte(anAddr + 2, (dnData & 0x00FF));
+}
+
+void MOVEP::LongRegToMem(uint32_t anAddr, uint8_t dn) {
+    uint32_t dnData = cpu->getDataRegister(dn);
+    for (int i = 0; i < 4; i++) {
+        uint32_t shift = (3 - i) * 8;
+        uint32_t mask = (0xFFu) << shift;
+        uint32_t writeAddr = anAddr + (2 * i);
+        bus->writeByte(writeAddr, (dnData & mask) >> shift);
+    }
 }
