23364b43c795cf2c813a2d4cba214c187d2f4218
Paul Trampert <paul.trampert@ptrampert.com>
2021-09-18 01:23:05 -0400

    Implement MOVEA

---
+++ include/GenieSys/CpuOperations/MOVEA.h
@@ -0,0 +1,21 @@
+//
+// Created by pault on 9/18/2021.
+//
+
+#pragma once
+#include "./CpuOperation.h"
+
+class MOVEA : public CpuOperation {
+private:
+    BitMask<uint16_t> sizeMask = BitMask<uint16_t>(13, 2, 2, 3);
+    BitMask<uint16_t> destMask = BitMask<uint16_t>(11, 3);
+    BitMask<uint16_t> eaModeMask = BitMask<uint16_t>(5, 3);
+    BitMask<uint16_t> eaAddrMask = BitMask<uint16_t>(2, 3);
+
+public:
+    MOVEA(M68kCpu* cpu, Bus* bus);
+    uint8_t getSpecificity() override;
+    std::vector<uint16_t> getOpcodes() override;
+    uint8_t execute(uint16_t opWord) override;
+    std::string disassemble(uint16_t opWord) override;
+};
