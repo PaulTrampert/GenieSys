6212b483fab2f1aa9aaea289ea0f36447465362d
Paul Trampert <paul.trampert@ptrampert.com>
2022-02-28 19:54:14 -0500

    Implemented RTS
    Closes #45

---
+++ include/GenieSys/CpuOperations/RTS.h
@@ -0,0 +1,21 @@
+//
+// Created by paul on 2/28/22.
+//
+
+#pragma once
+#include "./CpuOperation.h"
+
+namespace GenieSys {
+    class RTS : public CpuOperation {
+    private:
+        uint16_t opWord = 0b0100111001110101;
+
+    public:
+        RTS(M68kCpu *cpu, Bus *bus);
+
+        std::vector<uint16_t> getOpcodes() override;
+        uint8_t getSpecificity() override;
+        uint8_t execute(uint16_t _) override;
+        std::string disassemble(uint16_t _) override;
+    };
+}
